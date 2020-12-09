db674946fd6a58fe22e235f78d50c114076adb56
maojianyu <mao@qq.com>
2020-12-09 15:05:40 +0800

    s

---
+++ app/api/controller/Spider.php
@@ -0,0 +1,34 @@
+<?php
+ /* 
+  * Copyright (C) 2017 All rights reserved.
+ *   
+ * @File UserTest.php
+ * @Brief 
+ * @Author 毛子
+ * @Version 1.0
+ * @Date 2017-12-26
+ * @Remark 服务端接口
+ */
+namespace app\api\controller;
+use think\Config;
+
+class Spider extends Common{
+    
+    public function _initialize() {
+        parent::_initialize();
+    }
+    
+    public function index() {
+        \think\Loader::import('Simpledom.simple_html_dom');
+        $html = file_get_html("http://www.315lz.com/date/2020/12");
+        $h4Elements = $html->find('<div class="bunnypresslite_rpimg_in">');
+        print_r($h4Elements);
+        exit;
+        
+        
+        
+        
+        return $this->fetch("",[]);
+    }
+    
+}
