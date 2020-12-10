d1905b1e690fb81e11aa7e6c0ff7d1ffe96e3b39
maojianyu <mao@qq.com>
2020-12-10 10:50:56 +0800

    s

--- app/common.php
+++ app/common.php
@@ -1187,3 +1187,42 @@ function mz_flow($uid, $oid="", $type, $money, $des, $balance) {
         "balance"=>$balance ? $balance : 0
     ));
 }
+
+function mz_getImage($imgUrl, $saveDir = './', $fileName = null) {
+    if (empty($imgUrl)) {
+        return false;
+    }
+    //获取图片信息大小
+    $imgSize = getImageSize($imgUrl);
+    if (!in_array($imgSize['mime'], array('image/jpg', 'image/gif', 'image/png', 'image/jpeg'), true)) {
+        return false;
+    }
+
+    //获取后缀名
+    $_mime = explode('/', $imgSize['mime']);
+    $_ext = '.' . end($_mime);
+
+    if (empty($fileName)) {  //生成唯一的文件名
+        $fileName = time()."_".mt_rand(0,1000). $_ext;
+    }
+
+    //开始攫取
+    ob_start();
+    readfile($imgUrl);
+    $imgInfo = ob_get_contents();
+    ob_end_clean();
+    if (!file_exists($saveDir)) {
+        mkdir($saveDir, 0777, true);
+    }
+    $fp = fopen($saveDir . $fileName, 'a');
+    $imgLen = strlen($imgInfo);    //计算图片源码大小
+    $_inx = 1024;   //每次写入1k
+    $_time = ceil($imgLen / $_inx);
+
+    for ($i = 0; $i < $_time; $i++) {
+        fwrite($fp, substr($imgInfo, $i * $_inx, $_inx));
+    }
+    fclose($fp);
+    return array('file_name' => $fileName, 'save_path' => $saveDir . $fileName);
+}
+
